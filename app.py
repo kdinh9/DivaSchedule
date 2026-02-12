a7a6935e5edf69039658bbcceffbe78def0bbf5d
kdinh9 <150653980+kdinh9@users.noreply.github.com>
2026-02-12 01:23:09 -0600

    Refactor app.py for better data handling and UI
    Updated schedule file name and improved date formatting. Adjusted player name formatting and modified default selection index for date dropdown.

--- app.py
+++ app.py
@@ -2,25 +2,36 @@ import streamlit as st
 import pandas as pd
 from filter_friend_finder import friendFinder, friends2Teams
 
+#
+SCHEDULE_FILE = '2026Spring_Schedule.csv'
+
+st.title("Locate Your Friends :heart: \n RIP my boy TDO :skull::leg::boom:")
+st.set_page_config(
+    page_title="Locate Your Friends",
+    page_icon='👬'
+)
+
+
 # Import Data
     # Schedule Data
-data = pd.read_csv("output.csv")
+data = pd.read_csv(SCHEDULE_FILE)
 data['Time'] = pd.to_datetime(data['Time'], format= '%I:%M %p').dt.time
-data['Date'] = pd.to_datetime(data['Date'], format='%a, %b %d').dt.strftime('%m-%d')
+data["Date"] = pd.to_datetime(data["Date"], format="%a, %b %d %Y").dt.strftime("%m-%d-%Y")
     # Player-Team Data
 column_names = ['Player','Division','Team Number']
 players_df = pd.read_csv("26springteams.csv", names = column_names, skiprows=1,dtype=str)
+parts = players_df['Player'].str.split(' ', n=1, expand=True)
+players_df['Player'] = parts[0] + ' ' + parts[1].str[:1]
 
-st.title("Watch Your Friends :heart: \n RIP my boy TDO :skull::leg::boom:")
+
 ### Drop Downs
     # Default Value 
-DEFAULT_FILTER_VALUE = "Select a category"
     # Date Dropdowns
 date_options = data['Date'].unique().tolist()
 selected_date = st.selectbox(
     'Select Date to Filter',
     options=date_options,
-    index=3
+    index=2
 )
     # Player Dropdown
 players_options = players_df['Player'].unique().tolist()
@@ -30,16 +41,9 @@ selected_players = st.multiselect(
     options=players_options,
     max_selections=15,
     accept_new_options=True,
-    default=["Junior Riengxay","Utmy Tran","Megan Silavongsa","Kevin Dinh","TonyTam Dinh","Pete Visounnaraj","Christopher Nguyen","Jay Bui", "Kristine Vital","Olivia Cunningham"]
+    #default=["Junior Riengxay","Utmy Tran","Megan Silavongsa","Kevin Dinh","TonyTam Dinh","Pete Visounnaraj","Christopher Nguyen","Jay Bui", "Kristine Vital"]
 )
-
-
-# # Filter
-# selected_players = ["Kevin Dinh","TonyTam Dinh", "Megan Silavongsa","Pete Visounnaraj", "Tommy Tran","Christine Trinh"]
-# selected_players = [s.lower() for s in selected_players]
-
 
 filtered_search = friendFinder(friends2Teams(players_df,selected_players),data,selected_date)
 st.write(filtered_search)
 # # st.write(selected_players)
-
