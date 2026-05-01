de838582644f1d06a713dde8a1c5e66de4f8103f
kdinh9 <150653980+kdinh9@users.noreply.github.com>
2026-04-30 23:27:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ date_options = data['Date'].unique().tolist()
 selected_date = st.selectbox(
     'Select Date to Filter',
     options=date_options,
-    index=10
+    index=0
 )
     # Player Dropdown
 players_options = players_df['Player'].unique().tolist()
