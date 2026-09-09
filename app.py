72ec039520d8386f20b379971e211bf1a8ebb5a6
kdinh9 <150653980+kdinh9@users.noreply.github.com>
2026-09-09 10:01:47 -0500

    Update date Index for Week 3

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ date_options = schedule_df['DATE'].unique().tolist()
 selected_date = st.selectbox(
     'Date',
     options=date_options,
-    index=1
+    index=2
 )
     # Player Dropdown
 players_options = players_df['PLAYER'].unique().tolist()
