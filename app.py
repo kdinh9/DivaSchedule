68f76f91f12a9a2b5bd1eab466fbeaf62bfac241
kdinh9 <150653980+kdinh9@users.noreply.github.com>
2026-09-17 21:36:19 -0500

    Update date selection index in Streamlit app

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ date_options = schedule_df['DATE'].unique().tolist()
 selected_date = st.selectbox(
     'Date',
     options=date_options,
-    index=2
+    index=3
 )
     # Player Dropdown
 players_options = players_df['PLAYER'].unique().tolist()
